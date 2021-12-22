e318794316ae5268b426e52d88797aa54f7c6da8
Fernandes Macedo <masedos@gmail.com>
2021-12-22 21:31:10 +0000

    day one

---
+++ Day_001/day-1-1-solution.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+__version__ = '0.0.1'
+__author__ = 'Fernandes Macedo'
+__email__ = 'masedos@gmail.com'
+
+print("""Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')
+""")
