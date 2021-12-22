7a5b65d1b2c5e68b5267908c7c26f97c15516880
Fernandes Macedo <masedos@gmail.com>
2021-12-22 17:54:34 +0000

    first commit

---
+++ Day_001/band-name-generator-end.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+__version__ = '0.0.1'
+__author__ = 'Fernandes Macedo'
+__email__ = 'masedos@gmail.com'
+
+print("Welcome to the Band Name Generator")
+city = input("What's name of the city you grew up in? \n" )
+pet = input("What's your pt's name? \n" )
+print(f"Your band name could be {city}  {pet}" )
