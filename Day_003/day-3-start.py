83c2e960d017723224d113a6d376d9293d7f7339
Fernandes Macedo <masedos@gmail.com>
2021-12-25 19:45:36 +0000

    Day 003

---
+++ Day_003/day-3-start.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+__version__ = '0.0.1'
+__author__ = 'Fernandes Macedo'
+__email__ = 'masedos@gmail.com'
+
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height > 120:
+  print('Yu can ride the rollercoaster')
+else:
+  print("You can't ride the rollercoaster")
