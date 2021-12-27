5c13ec9127e98632ce8ceedcecea3921a02c8750
Fernandes Macedo <masedos@gmail.com>
2021-12-27 15:22:17 +0000

    day-4-2-exercise

--- Day_004/day-4-2-exercise.py
+++ Day_004/day-4-2-exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 __author__ = 'Fernandes Macedo'
 __email__ = 'masedos@gmail.com'
 
@@ -15,3 +15,7 @@ names = names_string.split(", ")
 import random
 
 print(f"{random.choice(names)} is going to buy the meal today!")
+
+
+name = random.randint(0, len(names) - 1)
+print(f"{names[name]} is going to buy the meal today!")
