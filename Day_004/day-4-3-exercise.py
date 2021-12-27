3675ad6de84ce26d5e02142504c5447cd7dfa16e
Fernandes Macedo <masedos@gmail.com>
2021-12-27 16:32:18 +0000

    day-4-3-exercise

---
+++ Day_004/day-4-3-exercise.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+__version__ = '0.0.1'
+__author__ = 'Fernandes Macedo'
+__email__ = 'masedos@gmail.com'
+
+
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = list(input("Where do you want to put the treasure? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+colum = int(position[0])
+row = int(position[1])
+
+map[row - 1] [colum - 1] = "X"
+
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-Day_003#!/usr/bin/env python
+#!/usr/bin/env python
 
 __version__ = '0.0.1'
 __author__ = 'Fernandes Macedo'
@@ -7,6 +7,4 @@ __email__ = 'masedos@gmail.com'
 
 #Write your code below this line 👇
 
-print("Hello " + input("What is your name? "))
 
-print(len(input("What is your name? " )))
