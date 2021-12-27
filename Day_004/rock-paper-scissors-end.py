8b0e0e2abe4f250e8254b86a6f6871b1e4fb4b0f
Fernandes Macedo <masedos@gmail.com>
2021-12-27 12:13:37 +0000

    Day 004

---
+++ Day_004/rock-paper-scissors-end.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+__version__ = '0.0.1'
+__author__ = 'Fernandes Macedo'
+__email__ = 'masedos@gmail.com'
+
+
+choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
