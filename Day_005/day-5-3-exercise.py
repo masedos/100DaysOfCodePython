1eb0078d1dd645f195aea69023732bdd7b398035
Fernandes Macedo <masedos@gmail.com>
2021-12-29 12:58:27 +0000

    day-5-3-exercise

--- Day_005/day-5-3-exercise.py
+++ Day_005/day-5-3-exercise.py
@@ -5,7 +5,9 @@ __author__ = 'Fernandes Macedo'
 __email__ = 'masedos@gmail.com'
 
 
+#Write your code below this row 👇
+
 total = 0
-for number in range(0, 101):
+for number in range(2, 101, 2):
   total += number
 print(total)
