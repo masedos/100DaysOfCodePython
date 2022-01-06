1c7ff321f8433570f0a9f43d25ce3f5d80057cc6
Fernandes Macedo <masedos@gmail.com>
2022-01-06 18:43:14 +0000

    day-8-start

---
+++ Day_008/day-8-start.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+__version__ = '0.0.1'
+__author__ = 'Fernandes Macedo'
+__email__ = 'masedos@gmail.com'
+
+
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+def greet():
+  print("Statement 1")
+  print("Statement 2")
+  print("Statement 3")
+greet()
